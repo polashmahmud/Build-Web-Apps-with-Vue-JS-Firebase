7ab3759e45a5b2a4bf524c128097650acb6bbc21
Polash Mahmud <polashmahmud@gmail.com>
2022-02-04 22:02:54 +0600

    complet basic-4

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const app = Vue.createApp({
     data() {
         return {
-
+            title: "This is title",
+            age: 47
+        }
+    },
+    methods: {
+        changeTitle(title) {
+            this.title = title;
         }
     }
 });

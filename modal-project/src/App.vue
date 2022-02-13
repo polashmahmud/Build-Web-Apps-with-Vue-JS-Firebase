0e933e90b37edbf8e605f76963d147e017c4f639
Polash Mahmud <polashmahmud@gmail.com>
2022-02-13 13:11:23 +0600

    modify app file

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <h1>{{ title }}</h1>
+
+  <div @click="handleClick" ref="box" class="box"></div>
+  <input type="text" ref="name" placeholder="name" style="margin-top: 20px" />
 </template>
 
 <script>
@@ -10,8 +13,25 @@ export default {
       title: "This is my first title",
     };
   },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.box);
+      this.$refs.box.classList.add("active");
+      this.$refs.name.focus();
+    },
+  },
 };
 </script>
 
 <style>
+.box {
+  width: 200px;
+  height: 200px;
+  cursor: pointer;
+  background: red;
+}
+
+.box.active {
+  background: blue;
+}
 </style>
